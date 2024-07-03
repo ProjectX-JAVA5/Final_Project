7cbc38cd041603f378ac4630f0707914d9bb90fb
J0YERIM <chosarah70@gmail.com>
2024-07-03 12:00:29 +0900

    feat: ReservationStatus 변경

--- src/main/java/com/projectX/ChargerReserv/domain/reservation/entity/ReservationStatus.java
+++ src/main/java/com/projectX/ChargerReserv/domain/reservation/entity/ReservationStatus.java
@@ -5,7 +5,7 @@ package com.projectX.ChargerReserv.domain.reservation.entity;
  */
 public enum ReservationStatus {
     PENDING,
-    CONFORMING,
+    CONFIRMED,
     CANCELLED,
     FAILED,
     COMPLETED
