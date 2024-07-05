5512c29232bd43eda4468b3c76e0c99c08782d0a
J0YERIM <chosarah70@gmail.com>
2024-07-05 10:33:35 +0900

    feat: 예약 생성 기능 구현(동시성 제어 구현X)

---
+++ src/main/java/com/projectX/ChargerReserv/domain/reservation/dto/CreateReservationRequest.java
@@ -0,0 +1,18 @@
+package com.projectX.ChargerReserv.domain.reservation.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record CreateReservationRequest(
+        @NotBlank(message = "차량 번호는 필수입니다.") String vehicleNumber,
+        @NotNull(message = "충전기 ID는 필수입니다.") Long chargerId
+) {
+
+    public CreateReservationCommand toCommand(Long userId) {
+        return CreateReservationCommand.builder()
+                .userId(userId)
+                .vehicleNumber(vehicleNumber)
+                .chargerId(chargerId)
+                .build();
+    }
+}
--- src/main/java/com/projectX/ChargerReserv/domain/user/repository/UserRepository.java
+++ src/main/java/com/projectX/ChargerReserv/domain/user/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.projectX.ChargerReserv.domain.user.repository;
 
 import com.projectX.ChargerReserv.domain.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     UserEntity findByOuthId(Long outhId);
 }
