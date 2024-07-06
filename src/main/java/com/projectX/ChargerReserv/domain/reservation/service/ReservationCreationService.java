e2d6bdce07781ba2127d2ed691d145f35242b4de
J0YERIM <chosarah70@gmail.com>
2024-07-06 12:03:40 +0900

    feat: 예약 확인 기능 구현 및 예약 생성/예약 확인 유효성 검사 추가

--- src/main/java/com/projectX/ChargerReserv/domain/reservation/service/ReservationCreationService.java
+++ src/main/java/com/projectX/ChargerReserv/domain/reservation/service/ReservationCreationService.java
@@ -1,12 +1,15 @@
 package com.projectX.ChargerReserv.domain.reservation.service;
 
 import com.projectX.ChargerReserv.domain.charger.entity.ChargerEntity;
+import com.projectX.ChargerReserv.domain.charger.entity.ChargerStatus;
 import com.projectX.ChargerReserv.domain.charger.repository.ChargerRepository;
 import com.projectX.ChargerReserv.domain.reservation.dto.CreateReservationCommand;
 import com.projectX.ChargerReserv.domain.reservation.entity.ReservationEntity;
+import com.projectX.ChargerReserv.domain.reservation.entity.ReservationStatus;
 import com.projectX.ChargerReserv.domain.reservation.repository.ReservationRepository;
 import com.projectX.ChargerReserv.domain.user.entity.UserEntity;
 import com.projectX.ChargerReserv.domain.user.repository.UserRepository;
+import com.projectX.ChargerReserv.global.error.IllegalArgumentException;
 import com.projectX.ChargerReserv.global.error.NoExistException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -26,7 +29,24 @@ public class ReservationCreationService {
                 .orElseThrow(() -> new NoExistException("사용자를 찾을 수 없습니다."));
         ChargerEntity charger = chargerRepository.findById(command.chargerId())
                 .orElseThrow(() -> new NoExistException("충전기를 찾을 수 없습니다."));
+        validateReservation(user, charger);
         ReservationEntity reservation = ReservationFactory.create(command.vehicleNumber(), user, charger);
         return reservationRepository.save(reservation).getId();
+    }
+
+    private void validateReservation(UserEntity user, ChargerEntity charger) {
+        if (charger.getStatus() != ChargerStatus.AVAILABLE) {
+            throw new IllegalArgumentException("사용할 수 없는 충전기입니다.");
+        }
+
+        boolean chargerHasPendingReservations = reservationRepository.existsByChargerAndStatus(charger, ReservationStatus.PENDING);
+        if (chargerHasPendingReservations) {
+            throw new IllegalStateException("해당 충전기에 대기 중인 예약이 있습니다.");
+        }
+
+        boolean userHasActiveReservations = reservationRepository.existsByUserAndStatus(user, ReservationStatus.PENDING);
+        if (userHasActiveReservations) {
+            throw new IllegalArgumentException("사용자가 이미 활성화된 예약을 가지고 있습니다.");
+        }
     }
 }
