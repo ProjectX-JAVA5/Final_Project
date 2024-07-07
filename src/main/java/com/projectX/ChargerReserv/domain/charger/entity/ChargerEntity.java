b46640b771be1a85268e561c317b4ef9fb17b410
J0YERIM <chosarah70@gmail.com>
2024-07-07 17:53:37 +0900

    feat: 충전기 예약 상태 추가 및 충전기에 Version 추가 및 예약 생성 수정

--- src/main/java/com/projectX/ChargerReserv/domain/charger/entity/ChargerEntity.java
+++ src/main/java/com/projectX/ChargerReserv/domain/charger/entity/ChargerEntity.java
@@ -32,4 +32,39 @@ public class ChargerEntity extends BasicEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="ChargingStation_id")
     private ChargingStationEntity station;
+
+    @Version
+    private Long version;
+
+    public void reserve() {
+        if (this.status == ChargerStatus.AVAILABLE) {
+            this.status = ChargerStatus.RESERVED;
+        } else {
+            throw new IllegalStateException("사용할 수 없는 충전기입니다.");
+        }
+    }
+
+    public void start() {
+        if (this.status == ChargerStatus.RESERVED) {
+            this.status = ChargerStatus.CHARGING;
+        } else {
+            throw new IllegalStateException("충전을 시작할 수 없는 상태입니다.");
+        }
+    }
+
+    public void cancel() {
+        if (this.status == ChargerStatus.RESERVED) {
+            this.status = ChargerStatus.AVAILABLE;
+        } else {
+            throw new IllegalStateException("예약을 취소할 수 없는 상태입니다.");
+        }
+    }
+
+    public void complete() {
+        if (this.status == ChargerStatus.CHARGING) {
+            this.status = ChargerStatus.AVAILABLE;
+        } else {
+            throw new IllegalStateException("충전을 완료할 수 없는 상태입니다.");
+        }
+    }
 }
--- src/main/java/com/projectX/ChargerReserv/domain/reservation/service/ReservationCreationService.java
+++ src/main/java/com/projectX/ChargerReserv/domain/reservation/service/ReservationCreationService.java
@@ -10,7 +10,6 @@ import com.projectX.ChargerReserv.domain.reservation.repository.ReservationRepos
 import com.projectX.ChargerReserv.domain.user.entity.UserEntity;
 import com.projectX.ChargerReserv.domain.user.repository.UserRepository;
 import com.projectX.ChargerReserv.global.config.RabbitMQConfig;
-import com.projectX.ChargerReserv.global.error.IllegalArgumentException;
 import com.projectX.ChargerReserv.global.error.NoExistException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -25,8 +24,6 @@ import org.springframework.stereotype.Service;
 @Transactional
 @Service
 public class ReservationCreationService {
-
-    private static final int RESERVATION_EXPIRATION_DELAY_MS = 30 * 60 * 1000;
 
     private final ReservationRepository reservationRepository;
     private final UserRepository userRepository;
@@ -45,36 +42,38 @@ public class ReservationCreationService {
         ChargerEntity charger = chargerRepository.findById(command.chargerId())
                 .orElseThrow(() -> new NoExistException("충전기를 찾을 수 없습니다."));
 
-        validateReservation(user, charger);
+        checkIfChargerAvailable(charger);
+        checkUserActiveReservations(user);
 
-        ReservationEntity reservation = ReservationFactory.create(command.vehicleNumber(), user, charger);
-        reservation = reservationRepository.save(reservation);
+        changeChargerStatusToReserved(charger);
 
+        ReservationEntity reservation = createAndSaveReservation(command, user, charger);
         sendExpirationMessage(reservation.getId());
 
         return reservation.getId();
     }
 
-    /**
-     * 예약의 유효성을 검사합니다.
-     *
-     * @param user    예약을 생성하는 사용자.
-     * @param charger 예약에 사용할 충전기.
-     */
-    private void validateReservation(UserEntity user, ChargerEntity charger) {
+    private void checkIfChargerAvailable(ChargerEntity charger) {
         if (charger.getStatus() != ChargerStatus.AVAILABLE) {
-            throw new IllegalArgumentException("사용할 수 없는 충전기입니다.");
+            throw new IllegalStateException("충전기가 예약 가능한 상태가 아닙니다.");
         }
+    }
 
-        boolean chargerHasPendingReservations = reservationRepository.existsByChargerAndStatus(charger, ReservationStatus.PENDING);
-        if (chargerHasPendingReservations) {
-            throw new IllegalStateException("해당 충전기에 대기 중인 예약이 있습니다.");
+    private void checkUserActiveReservations(UserEntity user) {
+        boolean hasActiveReservations = reservationRepository.existsByUserAndStatus(user, ReservationStatus.PENDING);
+        if (hasActiveReservations) {
+            throw new IllegalStateException("사용자가 이미 활성화된 예약을 가지고 있습니다.");
         }
+    }
 
-        boolean userHasActiveReservations = reservationRepository.existsByUserAndStatus(user, ReservationStatus.PENDING);
-        if (userHasActiveReservations) {
-            throw new IllegalArgumentException("사용자가 이미 활성화된 예약을 가지고 있습니다.");
-        }
+    private void changeChargerStatusToReserved(ChargerEntity charger) {
+        charger.reserve();
+        chargerRepository.save(charger);
+    }
+
+    private ReservationEntity createAndSaveReservation(CreateReservationCommand command, UserEntity user, ChargerEntity charger) {
+        ReservationEntity reservation = ReservationFactory.create(command.vehicleNumber(), user, charger);
+        return reservationRepository.save(reservation);
     }
 
     private void sendExpirationMessage(Long reservationId) {
